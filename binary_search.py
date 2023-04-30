4d42c68d324dc740baa39053858e2b73d60a9b6e
Karen33434 <98403088+Tasoka123ji@users.noreply.github.com>
2023-04-30 20:58:10 +0400

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -10,10 +10,10 @@ def binary_search_recursion(arr: list, target: int,lenght = None) -> int:
         return False 
     
     if target < arr[mid]:
-        return binary_search(arr[:mid+1],target,lenght - len(arr)//4)
+        return binary_search_recursion(arr[:mid+1],target,lenght - len(arr)//4)
     
     if target > arr[mid]:
-        return binary_search(arr[mid:],target,lenght+len(arr)//4)
+        return binary_search_recursion(arr[mid-1:],target,lenght+len(arr)//4)
    
     
 
@@ -39,4 +39,4 @@ def binary_search(arr,target):
 
 arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27]
 
-print(binary_search(arr,6))
+print(binary_search_recursion(arr,7))
